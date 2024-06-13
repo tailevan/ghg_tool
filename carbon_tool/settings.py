7c20b8a0be9dc430c50426ee57aa55abb7926c90
tailevan <tai.lv@hotmail.com>
2024-06-13 17:22:14 +0700

    csrf token change the cookies setting for https

--- carbon_tool/settings.py
+++ carbon_tool/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY = 'django-insecure--+votlu01w%4gs!9jc39@t3mkm%6w6e7+b%u$70iqapzsta*ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
+# Cookies settings for https for localhost should change to false
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 ALLOWED_HOSTS = [".awsapprunner.com",
                  "localhost",
                  "wssghg.fly.dev",
