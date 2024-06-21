8b2f50e7e51bbe31ddad1e45e7b6192757b52331
tailevan <tai.lv@hotmail.com>
2024-06-21 13:43:11 +0700

    change database backend

--- carbon_tool/settings.py
+++ carbon_tool/settings.py
@@ -109,13 +109,6 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-if 'FLY_ENV' in os.environ:
-    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
@@ -123,6 +116,7 @@ if 'FLY_ENV' in os.environ:
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
